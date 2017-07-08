d2269064fedd33fe0196d0445c43b03a3fcbd55e
Thomas Williams <thomas.williams@live.co.uk>
2017-07-08 21:16:19 +0200

    Adding function (and tests thereof) to determine if an integer is amicable.

--- Problem_021/src/Problem_021.cpp
+++ Problem_021/src/Problem_021.cpp
@@ -30,6 +30,20 @@ int d_of_n(int n) {
 	return sum;
 }
 
+bool is_amicable(int a) {
+	int d_of_a = d_of_n(a);
+	int d_of_b = d_of_n(d_of_a);
+//	cout << "########" << endl;
+//	cout << "d(a) with a=" << a << " is " << d_of_a << endl;
+//	cout << "d(b) with b=" << d_of_a << " is " << d_of_b << endl;
+//	cout << (a != d_of_a ? "true" : "false") << endl;
+//	cout << "########" << endl;
+	if( (a != d_of_a) && a == d_of_b ) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 int main() {
 	cout << "Project Euler | Problem 21\n" << endl;
@@ -39,6 +53,13 @@ int main() {
 	d_of_n(2);
 	d_of_n(6);
 	d_of_n(220);
+
+	cout << "Testing 1." << endl;
+	d_of_n(d_of_n(220));
+
+	cout << "Testing 2. Is amicable?" << endl;
+	cout << (is_amicable(220) ? "true" : "false") << endl;
+	cout << (is_amicable(284) ? "true" : "false") << endl;
 
 	return 0;
 }
