d6d095fc5be9722565f4a7261328e2b0b5b6202b
Thomas Williams <thomas.williams@live.co.uk>
2017-07-09 12:43:45 +0200

    Adding debug comments for is amicable.

--- Problem_021/src/Problem_021.cpp
+++ Problem_021/src/Problem_021.cpp
@@ -36,15 +36,20 @@ int d_of_n(int n, bool debug) {
 	return sum;
 }
 
-bool is_amicable(int a) {
-	int d_of_a = d_of_n(a);
-	int d_of_b = d_of_n(d_of_a);
-//	cout << "########" << endl;
-//	cout << "d(a) with a=" << a << " is " << d_of_a << endl;
-//	cout << "d(b) with b=" << d_of_a << " is " << d_of_b << endl;
-//	cout << (a != d_of_a ? "true" : "false") << endl;
-//	cout << "########" << endl;
-	if( (a != d_of_a) && a == d_of_b ) {
+// Returns true if a is an amicable number, false otherwise.
+// set debug = true for helpful comments when debugging.
+bool is_amicable(int a, bool debug) {
+	int d_of_a = d_of_n(a, 0);
+	int d_of_b = d_of_n(d_of_a, 0);
+	if(debug) {
+		cout << "########" << endl;
+		cout << "d(a) with a=" << a << " is " << d_of_a << endl;
+		cout << "d(b) with b=" << d_of_a << " is " << d_of_b << endl;
+		cout << "a not equal to b? : " << (a != d_of_a ? "true" : "false") << endl;
+		cout << "a = d(b) ==> " << a << " = " << d_of_b << " ? : " << (a == d_of_b ? "true" : "false") << endl;
+		cout << "########" << endl;
+	}
+	if ((a != d_of_a) && a == d_of_b) {
 		return true;
 	} else {
 		return false;
