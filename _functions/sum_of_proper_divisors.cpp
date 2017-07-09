8276d686f37439bc122a3874d19e5323897a81e7
Thomas Williams <thomas.williams@live.co.uk>
2017-07-09 21:08:05 +0200

    Adding new _functions folder to save functions that can be used in different projects.

---
+++ _functions/sum_of_proper_divisors.cpp
@@ -0,0 +1,33 @@
+// Returns -1 if n <= 1, else returns the sum of the
+// proper divisors of n
+// set debug = true for helpful comments when debugging.
+int sum_of_proper_divisors(int n, bool debug) {
+	// for debugging
+	if (debug)
+		cout << "Finding d_of_n for n = " << n << endl;
+
+	if (n <= 1) {
+		cout << "n must be greater than 1!\n=======\n" << endl;
+		return -1;
+	}
+
+	long int sum = 1; // 1 is a divisor of all positive integers <= n, for n >= 2
+
+	if (debug)
+		cout << "Proper divisors: 1 ";
+
+	for (int i = 2; i < n; i++) {
+		if (!(((float) n / i) - n / i > 0)) {
+			if (debug)
+				cout << ", " << i;
+			sum += i;
+		}
+	}
+	if (debug) {
+		cout << endl;
+		cout << "d(" << n << ") = " << sum << endl;
+		cout << "=======" << endl;
+	}
+
+	return sum;
+}
