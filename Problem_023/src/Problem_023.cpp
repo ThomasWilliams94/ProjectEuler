14a03c6aa5ccb6146923d52fe7ddb0d9cda08543
Thomas Williams <thomas.williams@live.co.uk>
2017-07-09 21:17:40 +0200

    Adding first functions. sum_of_proper_divisors copied in from _functions, is_perfect new.

--- Problem_023/src/Problem_023.cpp
+++ Problem_023/src/Problem_023.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
 using namespace std;
 
+// Returns -1 if n <= 1, else returns the sum of the
+// proper divisors of n
+// set debug = true for helpful comments when debugging.
+int sum_of_proper_divisors(int n, bool debug = false) {
+	// for debugging
+	if (debug)
+		cout << "Finding d_of_n for n = " << n << endl;
+
+	if (n <= 1) {
+		cout << "n must be greater than 1!\n=======\n" << endl;
+		return -1;
+	}
+
+	long int sum = 1; // 1 is a divisor of all positive integers <= n, for n >= 2
+
+	if (debug)
+		cout << "Proper divisors: 1";
+
+	for (int i = 2; i < n; i++) {
+		if (!(((float) n / i) - n / i > 0)) {
+			if (debug)
+				cout << ", " << i;
+			sum += i;
+		}
+	}
+	if (debug) {
+		cout << endl;
+		cout << "d(" << n << ") = " << sum << endl;
+		cout << "=======" << endl;
+	}
+
+	return sum;
+}
+
+bool is_perfect(int n) {
+	if( sum_of_proper_divisors(n) == n) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 int main() {
 	cout << "Project Euler | Problem 23\n" << endl;
+
+	cout << (is_perfect(28) ? "true" : "false") << endl;
 	return 0;
 }
